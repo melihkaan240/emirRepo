a05a51f05e531680c65e3512ea98cb493d00dbdc
melihkaan240 <melihkaanarac@gmail.com>
2023-12-22 00:01:14 +0300

    fix: text

--- src/pages/Discovery.jsx
+++ src/pages/Discovery.jsx
@@ -3,30 +3,28 @@ import Navbar from '../components/navbar/Navbar'
 import "./discovery.css"
 export const Discovery = () => {
   return (
-   <div className="App">
-       <Navbar/>
-       <div>
-       <div className='mainTitle'>
-       <span className='mainTitleTextLeft'>Subfinder</span>
-       <span className='mainTitleTextRight'>Subfinder</span>
-       </div>
-       <div className='main'>
-       <div className='mainleft'>
-        <div className='leftChildContainer'>
-                  <p className='leftTextTitle'>Phantom</p>
-
-       <p className='leftText'>
-     ua. mco laboris nisi ut aliquip ex ea commodo consequat.
-       </p>
-         <button className='homeBtn'>Run</button>
-
-       </div>
-       </div>
-       <div className='mainright'>
-       <img src={'/discoveryPhoto.png'} alt="Home" />
-       </div>
-       </div>
-       </div>
+    <div className="App">
+    <Navbar/>
+    <div>
+    <div className='mainTitle'>
+     <span className='mainTitleText'>Subfinder</span>
+    </div>
+    <div className='main'>
+    <div className='mainleft'>
+     <div className='leftChildContainer'>
+    <p className='leftTextTitle'>Lorem ipsum dolor</p>
+    <p className='leftText'>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+    <button className='homeBtn'>Run</button>
+    
+    </div>
+    </div>
+    <div className='mainright'>
+    <img src={'/discoveryPhoto.png'} alt="Discovery" className='img' />
+    </div>
+    </div>
+    </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,28 +4,30 @@ import "./home.css"
 const Home = () => {
   return (
     <div className="App">
-       <Navbar/>
-       <div>
-       <div className='mainTitle'>
-        <span className='mainTitleText'>Subfinder</span>
-       </div>
-       <div className='main'>
-       <div className='mainleft'>
-        <div className='leftChildContainer'>
-       <p className='leftTextTitle'>Lorem ipsum dolor</p>
-       <p className='leftText'>
-       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-       </p>
-<button className='homeBtn'>Run</button>
-
-       </div>
-       </div>
-       <div className='mainright'>
-       <img src={'/homePhoto.png'} alt="Home" className='img' />
-       </div>
-       </div>
-       </div>
+    <Navbar/>
+    <div>
+    <div className='mainTitle'>
+    <span className='mainTitleTextLeft'>Phantom</span>
+    <span className='mainTitleTextRight'>Pentest</span>
     </div>
+    <div className='main'>
+    <div className='mainleft'>
+     <div className='leftChildContainer'>
+               <p className='leftTextTitle'>Phantom</p>
+
+    <p className='leftText'>
+  ua. mco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum iusto cum rerum tenetur culpa! Maxime suscipit quo eaque numquam laboriosam?
+    </p>
+      <button className='homeBtn'>Run</button>
+
+    </div>
+    </div>
+    <div className='mainright'>
+    <img src={'/homePhoto.png'} alt="Home"  className='img' />
+    </div>
+    </div>
+    </div>
+ </div>
   )
 }
 
